11642887bb8763ae96bac97774354e4fc78f15cf
bkatz8 <bkatz237@gmail.com>
2014-04-12 15:15:50 -0400

    email
    changed site title

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
 $emailTo = 'bkatz237@gmail.com';
-$siteTitle = 'Benjamin Katz';
+$siteTitle = 'www.benjaminakatz.com';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
